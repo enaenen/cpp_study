9e61be957c5ff169e5777d99d11df95156628683
Woo Joo Chae <enaenen@naver.com>
2022-09-17 19:02:09 +0900

    castings

---
+++ casting/casting_test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main( void ) {
+	double *dbl = new double( 3.141592 );
+	// int *i1 = dbl;         //묵시적 변환
+	int *i2 = (int *)dbl;  //명시적 형변환
+	// int *i3 = static_cast<int *>( dbl );
+
+	// std::cout << i1, i2, i3 << std::endl;
+	std::cout << *dbl << std::endl;
+	std::cout << *i2 << std::endl;
+}
